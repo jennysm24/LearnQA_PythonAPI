60c32d5e6e4363ef99ea870f1d587bd7713bed09
Evgeniia Sadykova <e.sadykova@semrush.com>
2024-12-31 16:22:06 +0100

    homework 3.2

---
+++ tests/test_homework_3_2.py
@@ -0,0 +1,22 @@
+import requests
+import pytest
+
+class TestUserAuth:
+    expected_values = [
+        ("hw_value"),
+        ("hw_value1"),
+        ("")
+    ]
+
+    @pytest.mark.parametrize('expected_value', expected_values)
+    def test_auth_user(self, expected_value):
+        data = {'email': 'vinkotov@example.com',
+                'password': '1234'}
+
+        url = "https://playground.learnqa.ru/api/homework_cookie"
+
+        response = requests.get(url, params=data)
+        for cookie_name, cookie_value in response.cookies.items():
+            print(f"Cookie name, {cookie_name}, Cookie value: {cookie_value}")
+        assert "HomeWork" in response.cookies, "There is no HomeWork cookie in the response"
+        assert cookie_value == expected_value, "Actual value of cookie in the response is not correct"
