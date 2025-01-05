97a14954769c2fa375aa170a68e7efc268308bef
Evgeniia Sadykova <e.sadykova@semrush.com>
2025-01-05 17:05:23 +0100

    homework 4.3

---
+++ tests/test_homework_4_3.py
@@ -0,0 +1,134 @@
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+from lib.my_requests import MyRequests
+
+
+class TestUserEdit(BaseCase):
+    def test_edit_just_created_user(self):
+        # Register
+        register_data = self.prepare_registration_data()
+        # url = "https://playground.learnqa.ru/api/user/"
+        # response1 = requests.post(url, data=register_data)
+        response1 = MyRequests.post('/user/', data=register_data)
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        first_name = register_data['firstName']
+        password = register_data['password']
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {'email': email,
+                      'password': password
+                      }
+        # url2 = "https://playground.learnqa.ru/api/user/login"
+        # response2 = requests.post(url2, data=login_data)
+        response2 = MyRequests.post('/user/login', data=login_data)
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # EDIT
+        new_name = "Changed Name"
+        # url3 = f"https://playground.learnqa.ru/api/user/{user_id}"
+        # response3 = requests.put(url3,
+        #                          headers={"x-csrf-token":token},
+        #                          cookies={"auth_sid": auth_sid},
+        #                          data={"firstName": new_name}
+        #                          )
+        response3 = MyRequests.put(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"firstName": new_name}
+                                   )
+        Assertions.assert_code_status(response3, 200)
+
+        # GET
+        # url4 = f"https://playground.learnqa.ru/api/user/{user_id}"
+        # response4 = requests.get(url4,
+        #                          headers={"x-csrf-token": token},
+        #                          cookies={"auth_sid": auth_sid}
+        #                          )
+        response4 = MyRequests.get(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid}
+                                   )
+        Assertions.assert_json_value_by_name(response4,
+                                             "firstName",
+                                             new_name,
+                                             "Wrong name of the user after edit"
+                                             )
+
+    # 1
+    def test_edit_user_without_auth(self):
+        # EDIT
+        new_name = "Changed Name"
+        existing_user_id = 113522
+        response = MyRequests.put(f"/user/{existing_user_id}",
+                                  data={"firstName": new_name}
+                                  )
+        Assertions.assert_code_status(response, 400)
+        Assertions.assert_json_value_by_name(response, "error", "Auth token not supplied",
+                                             f"Expected 'Auth token not supplied', but got {response.text}")
+
+    # 2
+    def test_edit_user_with_auth_of_different_user(self):
+        # EDIT
+        new_name = "Changed Name"
+        existing_user_id = 113522
+        different_user_auth_sid = '838338485e41ae560af15da79127d6afe29c195f2eda777887f675f16e79b704'
+        different_user_token = '64ff3c6e73d6e7b18c2abf19a365f8953327179be29c195f2eda777887f675f16e79b704'
+        response = MyRequests.put(f"/user/{existing_user_id}",
+                                  data={"firstName": new_name},
+                                  headers={"x-csrf-token": different_user_token},
+                                  cookies={"auth_sid": different_user_auth_sid}
+                                  )
+        Assertions.assert_code_status(response, 200)
+        response2 = MyRequests.get(f"/user/{113522}",
+                                   headers={"x-csrf-token": different_user_token},
+                                   cookies={"auth_sid": different_user_auth_sid}
+                                   )
+        Assertions.assert_json_has_key(response2, "username")
+        actual_username = self.get_json_value(response2, "username")
+        expected_username = "learnqa"
+        Assertions.assert_json_value_not_expected_by_name(response2, "username", expected_username,
+                                                          f"Expected username to be '{expected_username}', but got "
+                                                          f"actual value '{actual_username}'")
+        Assertions.assert_json_has_not_key(response2, "email")
+        Assertions.assert_json_has_not_key(response2, "firstName")
+        Assertions.assert_json_has_not_key(response2, "lastName")
+
+    # 3
+    def test_edit_user_to_wrong_email(self):
+        # EDIT
+        new_email = "jennysmmail.ru"
+        user_id = 113503
+        auth_sid = '838338485e41ae560af15da79127d6afe29c195f2eda777887f675f16e79b704'
+        token = '64ff3c6e73d6e7b18c2abf19a365f8953327179be29c195f2eda777887f675f16e79b704'
+
+        response3 = MyRequests.put(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"email": new_email}
+                                   )
+        Assertions.assert_code_status(response3, 400)
+        Assertions.assert_json_value_by_name(response3, "error", "Invalid email format",
+                                             f"Expected 'Invalid email format', but got {response3.text}")
+
+    # 4
+    def test_edit_user_to_short_firstname(self):
+        # EDIT
+        new_firstname = "T"
+        user_id = 113503
+        auth_sid = '838338485e41ae560af15da79127d6afe29c195f2eda777887f675f16e79b704'
+        token = '64ff3c6e73d6e7b18c2abf19a365f8953327179be29c195f2eda777887f675f16e79b704'
+
+        response4 = MyRequests.put(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"firstName": new_firstname}
+                                   )
+        Assertions.assert_code_status(response4, 400)
+        Assertions.assert_json_value_by_name(response4, "error",
+                                             "The value for field `firstName` is too short",
+                                             f"Expected 'The value for field `firstName` is too short', but got {response4.text}")
